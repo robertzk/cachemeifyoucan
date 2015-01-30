e4d045627c74d709c39ca7d374a902b7aec5c3e8
RobertZK <technoguyrob@gmail.com>
2015-01-29 20:08:52 -0600

    check that df_cached also matches

--- inst/tests/test-data_integrity.R
+++ inst/tests/test-data_integrity.R
@@ -8,7 +8,7 @@ describe("data integrity", {
       df_ref <- cbind(batch_data(1:5, model_version, type), data.frame(new_col = rep(NA_real_, 5)))
       df_ref <- rbind(df_ref, batch_data(6:10, model_version, type, add_column = TRUE))
       cached_fcn(id = 5:1,  model_version, type)
-      cached_fcn(id = 1:10, model_version, type, add_column = TRUE)
+      df_cached <- without_rownames(cached_fcn(id = 1:10, model_version, type, add_column = TRUE))
       expect_equal(without_rownames(df_ref),
                    without_rownames(cached_fcn(id = 1:10, model_version, type)))
       no_check <- TRUE
