4372949fd70198a0f5b36d50e7da50cae7935bd7
Fei Ye <fei.ye@avantcredit.com>
2015-02-03 13:50:25 -0600

    the source of the evil is another caching layer within the to-be-cached function, now the test really works as expected

--- inst/tests/helper-expect_cached_batch_data.R
+++ inst/tests/helper-expect_cached_batch_data.R
@@ -10,7 +10,6 @@ expect_cached_batch_data <- function(expr) {
   df_db <- db2df(dbReadTable(dbconn, cachemeifyoucan:::table_name(prefix, list(version = model_version))), dbconn, "loan_id")
   # Clumsy to check matching database
   if (!exists('no_check', envir = environment(), inherits = FALSE) ) {
-    df_db <- df_db[match(df_ref$loan_id, df_db$loan_id), ]
     equalN <- sum(vapply(colnames(df_ref), 
       function(nn) if (is.logical(df_ref[[nn]]) || 
         is.character(df_ref[[nn]]) || is.factor(df_ref[[nn]])) 
@@ -20,12 +19,6 @@ expect_cached_batch_data <- function(expr) {
     expect_equal(equalN, ncol(df_ref))
   }
   if (exists('df_cached', envir = environment(), inherits = FALSE)) {
-    equalN <- sum(vapply(colnames(df_ref), 
-      function(nn) if (is.logical(df_ref[[nn]]) || 
-        is.character(df_ref[[nn]]) || is.factor(df_ref[[nn]])) 
-        all.equal(as.character(df_ref[[nn]]), df_cached[[nn]]) 
-      else 
-        all.equal(df_ref[[nn]], df_cached[[nn]]), logical(1))) 
-    expect_equal(equalN, ncol(df_ref))
+    expect_equal(df_cached, df_ref)
   }
 }
--- inst/tests/test-batch_data.R
+++ inst/tests/test-batch_data.R
@@ -11,8 +11,8 @@ batch_data <- function(loan_id, version, ...) avant::batch_data(loan_id, version
 describe("batch data", {
   test_that("it actually works for avant::batch_data", {
     expect_cached_batch_data({
-      df_ref <- batch_data(c(32835,32836), model_version)
-      df_cached <- cached_fcn(c(32835,32836), model_version)
+      df_ref <- batch_data(c(32835,32836), model_version, cache = FALSE)
+      df_cached <- cached_fcn(c(32835,32836), model_version, cache = FALSE)
       no_check <- TRUE
     })
   })
