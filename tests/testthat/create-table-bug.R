6e59936b5276f5755212853b7219b80c3fdc295f
Michael Church <michael.o.church@gmail.com>
2015-03-05 11:15:26 -0600

    remove outdated test

--- tests/testthat/create-table-bug.R
+++
@@ -1,8 +0,0 @@
-## pre-bugfix : this blows up with the "Create Table" bug. 
-
-go <- function () {
-    fn <- function (n) data.frame(n = n, x = 1:n, y = runif(n))
-    f2 <- cache(fn, key = 'n', salt = c(), con = "~/dev/cachemeifyoucan/tests/test-resources/database.yml", env = "test.local", prefix = "f2")
-
-    f2(4)
-}
