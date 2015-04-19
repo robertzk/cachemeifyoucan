4c613e76a07bb32e858895d7e8155a11160060d9
Kirill Sevastyanenko <kirillseva@gmail.com>
2015-04-19 18:13:40 -0400

    crufty test

--- tests/testthat/test-uncached.R
+++ tests/testthat/test-uncached.R
@@ -21,10 +21,37 @@ describe("uncached function", {
   })
 
   test_that('uncaching batch data is also a cached function, but with force', {
-    # First remove all tables in the local database.
     expect_cached({
       df_ref <- batch_data(1:5)
       df_cached <- uncached(cached_fcn)(key = 1:5, model_version, type)
+    })
+  })
+
+  test_that('uncached invalidates cache', {
+    with_connection(dbconn(), {
+      # clean up
+      lapply(dbListTables(conn), function(t) dbRemoveTable(conn, t))
+      # set up some initial data
+      df <- data.frame(id = c(1,2,3,4,5), value = c(T,T,F,F,F))
+      func <- function(id, value) if(missing(value)) df[id, ] else df[id,]
+      c_func <- cache(func, key = c(key = "id"), c("value"), con = conn, prefix = prefix)
+      # overwrite func!
+      func <- function(id, value) if(missing(value)) data.frame(id = id, value = NA) else data.frame(id = id, value = value)
+      cc_func <- cache(func, key = c(key = "id"), c("value"), con = conn, prefix = prefix)
+
+      # cached version works as expected
+      expect_equal(df[1,], c_func(1, "test"))
+      # both pre- and after-cache
+      expect_equal(df[1,], c_func(1, "test"))
+      # cc_func will also return the row from df because it'll think it's cached
+      expect_equal(df[1,], cc_func(1, "test"))
+      # but now we will call uncached! and we should overwrite the db
+      expect_equal(data.frame(id = 1, value = "test"), uncached(cc_func)(1, "test"))
+      # and now the c_func will also read the new value from cache, because we've used force
+      expect_equal(data.frame(id = 1, value = "test"), c_func(1, "test"))
+
+      # clean up again
+      lapply(dbListTables(conn), function(t) dbRemoveTable(conn, t))
     })
   })
 })
