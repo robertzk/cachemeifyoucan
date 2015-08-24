496e250d63e9923211bbe49e16678ddfc4ebb642
Peter Hurford <peter@peterhurford.com>
2015-08-24 17:14:44 -0500

    Add failing test for two keys

--- tests/testthat/test-batching.R
+++ tests/testthat/test-batching.R
@@ -24,3 +24,16 @@ test_that("it calls batchman::batch when uncached keys exceed batch size", {
     expect_true(env$called)
   })
 })
+
+test_that("it can call batchman::batch with two keys when needed (issue #72)", {
+  env <- list2env(list(called = FALSE))
+  package_stub("batchman", "batch", function(...) { browser(); env$called <- TRUE; fn }, {
+    two_key <- function(alpha_key, beta_key) {
+      mapply(sum, alpha_key, beta_key)
+    }
+    df_ref <- two_key(1:101, 1:101)
+    cached_two_key <- cache(two_key, key = c(key = c("alpha_key", "beta_key")), character(0))
+    df_cached <- cached_two_key(alpha_key = 1:101, beta_key = 1:101)
+  })
+  expect_true(env$called)
+})
