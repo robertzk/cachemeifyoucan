3dee394505b51a0896476db3bcaa6460fdfd9f6d
Khalid Harun <khalid.harun@avantcredit.com>
2015-12-03 13:55:55 -0600

    add test for migration; fix bugs

--- R/migrations.R
+++ R/migrations.R
@@ -67,12 +67,12 @@ update_cache_salt <- function(dbconn, prefix, old_salt, new_salt) {
   old_table_name <- cachemeifyoucan:::table_name(prefix, old_salt)
   new_table_name <- cachemeifyoucan:::table_name(prefix, new_salt)
 
-  old_shard_names <- get_shards_for_table(old_table_name)
+  old_shard_names <- get_shards_for_table(dbconn, old_table_name)
   new_shard_names <- generate_new_shard_names(new_table_name, length(old_shard_names))
 
   lapply(seq_along(new_shard_names), function(i) {
     message(sprintf("Copying shard from %s to %s.", old_shard_names[i], new_shard_names[i]))
-    copy_shard(old_shard_names[i], new_shard_names[i])
+    copy_shard(dbconn, old_shard_names[i], new_shard_names[i])
   })
 
   write_table_shard_map(dbconn, new_table_name, new_shard_names)
@@ -80,10 +80,12 @@ update_cache_salt <- function(dbconn, prefix, old_salt, new_salt) {
 
 copy_shard <- function(dbconn, old_shard_name, new_shard_name) {
   fields <- names(DBI::dbGetQuery(dbconn, sprintf("SELECT * FROM %s LIMIT 1", old_shard_name)))
-  id_col <- grep("^(.*_id|ids)$", fields, value = TRUE)
+  id_col <- grep("^(.*_id|ids?)$", fields, value = TRUE)
   if (length(id_col) > 1) stop("More than one possible id column found.")
   if (length(id_col) < 1) stop("No id column found in shard. Not able to add an index")
 
+  if (DBI::dbExistsTable(dbconn, new_shard_name)) stop("Shard table already exists. Can't create a new table.")
   query <- sprintf("CREATE TABLE %s AS TABLE %s", new_shard_name, old_shard_name)
-  add_index(dbconn, new_table_name, id_col)
+  DBI::dbGetQuery(dbconn, query)
+  add_index(dbconn, new_shard_name, id_col)
 }
