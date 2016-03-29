1a6d8b93b16f6b9de29c72a0f2f8250fbd8b347a
RobertZK <technoguyrob@gmail.com>
2016-03-29 16:27:47 -0500

    We have nothing

--- R/metadata.R
+++ R/metadata.R
@@ -13,11 +13,9 @@ get_cache_meta_data <- function(dbconn, table_names) {
 }
 
 serialize_to_string <- function(obj) {
-  rawToChar(serialize(obj, NULL, ascii = TRUE))
 }
 
 unserialize_from_string <- function(obj_str) {
-  unserialize(charToRaw(obj_str))
 }
 
 ensure_cache_metadata_table_exists <- function(dbconn) {
